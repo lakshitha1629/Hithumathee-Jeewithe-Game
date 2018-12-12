5913e5cd2bdf8596b2b1b2aaa43e4bf27bac778a
lakshitha perera <lakshitha1629@gmail.com>
2018-12-12 21:20:18 +0530

    Create New Game-Hithumathe Jeewithe

---
+++ hithumathe jeewithe game/Assets/Splash/SplashFade.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashFade : MonoBehaviour {
+
+	public Image splashImage;
+	public string loadLeval;
+
+	// Use this for initialization
+	// 
+	IEnumerator Start () {
+	
+		splashImage.canvasRenderer.SetAlpha(0.0f);
+
+		fadeIn();
+		yield return new WaitForSeconds(2.5f);
+		fadeOut();
+		yield return new WaitForSeconds(2.5f);
+		SceneManager.LoadScene(loadLeval);       
+      
+	}
+	void fadeIn(){
+		splashImage.CrossFadeAlpha(1.0f,1.5f,false);
+	}
+	void fadeOut(){
+		splashImage.CrossFadeAlpha(0.0f,2.5f,false);
+	}
+
+}
